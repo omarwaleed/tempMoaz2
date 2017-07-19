2d0faadc57ebc266fff1a6146f9f890001bcfcf5
omarwaleed <omarpersonalprofile@gmail.com>
2017-07-20 01:45:31 +0200

    sending homepaage file

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require('express');
 const Path = require('path');
 const app = express();
 
+// app.set('view engine', 'html');
+
 app.use(express.static(Path.join(__dirname, 'public')));
 
 app.get('/', function(req, res){
-	app.render('home', function(err, html){});
+	// app.render('home', function(err, html){});
+	res.sendFile(Path.join(__dirname, 'public/home.html'));
 })
 
 app.listen(3000, function(err){
